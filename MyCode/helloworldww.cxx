680418ab03b1a39cbdc38a289b1f5e08619b3630
wiktor-witkowski-000 <114095100+wiktor-witkowski-000@users.noreply.github.com>
2025-11-12 14:04:00 +0000

    Magnitude of vector

--- MyCode/helloworldww.cxx
+++ MyCode/helloworldww.cxx
@@ -1,9 +1,18 @@
 // Wiktor Witkowski 12/11/2025
 #include <iostream>
+#include <cmath>
 
 int main()
 {
     // print 'Hello World' to the terminal
 std::cout << "Hello World :)" << std::endl;
+
+// Computing magnitude of vector z = x + y
+float x = 7.5;
+float y = 3.4;
+float z2 = x*x + y*y;
+float mag_z = std::sqrt(z2);
+
+std::cout << mag_z << std::endl;
 return 0;
 }
