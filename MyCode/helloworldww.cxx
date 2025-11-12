df5f0c07d6ac20c446a130b6013c8c730aa21cb1
wiktor-witkowski-000 <114095100+wiktor-witkowski-000@users.noreply.github.com>
2025-11-12 14:35:28 +0000

    Preliminary Exercises code for assignment 1

--- MyCode/helloworldww.cxx
+++ MyCode/helloworldww.cxx
@@ -1,6 +1,16 @@
 // Wiktor Witkowski 12/11/2025
 #include <iostream>
 #include <cmath>
+#include <vector>
+
+float mag_vector(float a, float b){
+    // Function that takes as an input 2 components of a 2D vector
+    // and outputs the magnitude of the vector
+    float c2 = a*a + b*b;
+    float c = std::sqrt(c2);
+    std::cout << "Using function, magnitude of x = " << c << std::endl;
+return c;
+}
 
 int main()
 {
@@ -14,5 +24,10 @@ float z2 = x*x + y*y;
 float mag_z = std::sqrt(z2);
 
 std::cout << mag_z << std::endl;
+
+float x_function;
+x_function = mag_vector(x, y);
+
+
 return 0;
 }
